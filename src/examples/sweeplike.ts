25e736718759fee049a33598a82c0a129438b523
Marco Massarelli <60667061+ceoloide@users.noreply.github.com>
2024-11-30 15:55:24 +0000

    Update to KiCad 8

--- src/examples/sweeplike.ts
+++ src/examples/sweeplike.ts
@@ -3,10 +3,11 @@ import {ConfigExample} from "./index";
 const Sweeplike: ConfigExample = {
     label: "Sweep-like (minimal)",
     author: "jcmkk3",
-    value: `# \`U\` is a predefined unit of measure that means 19.05mm, which is MX spacing
+    value: `# U is a predefined unit of measure that means 19.05mm, which is MX spacing (u is 19.00mm)
 points:
   zones:
     matrix:
+      anchor.shift: [50,-100] # Fix KiCad placement
       columns:
         pinky:
         ring.key.stagger: 0.66U
@@ -32,39 +33,59 @@ points:
           key.name: thumb_reachy
 pcbs:
   simple_split:
+    template: kicad8
     footprints:
       keys:
-        what: mx
+        what: ceoloide/switch_mx
         where: true
         params:
           from: GND
           to: "{{name}}"
-          reverse: true
-          keycaps: true
+          reversible: true
+          solder: true
+          include_keycap: true
       mcu:
-        what: promicro
+        what: ceoloide/mcu_nice_nano
         where:
           - ref: matrix_inner_home
             shift: [1U, 0.5U]
-            rotate: -90
         params:
+          reversible: true
+          only_required_jumpers: true
           P7: matrix_pinky_top
+          P7_label: P7
           P18: matrix_ring_top
+          P18_label: P18
           P19: matrix_middle_top
+          P19_label: P19
           P20: matrix_index_top
+          P20_label: P20
           P21: matrix_inner_top
+          P21_label: P21
           P15: matrix_pinky_home
+          P15_label: P15
           P14: matrix_ring_home
+          P14_label: P14
           P16: matrix_middle_home
+          P16_label: P16
           P10: matrix_index_home
+          P10_label: P10
           P1: matrix_inner_home
+          P1_label: P1
           P2: matrix_pinky_bottom
+          P2_label: P2
           P3: matrix_ring_bottom
+          P3_label: P3
           P4: matrix_middle_bottom
+          P4_label: P4
           P5: matrix_index_bottom
+          P5_label: P5
           P6: matrix_inner_bottom
+          P6_label: P6
           P8: thumb_tucky
+          P8_label: P8
           P9: thumb_reachy
+          P9_label: P9
 `
 };
 
